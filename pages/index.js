a4f99f41748bef4bf6f32361f6bbdb281d699e1a
uniqueJackal5 <santoshadhikari486@gmail.com>
2022-07-19 13:36:22 +0545

    some changes

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,10 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import Link from 'next/link'
+import {BsGithub, BsYoutube} from 'react-icons/bs'
+import {FaInstagram} from 'react-icons/fa'
+
 
 export default function Home() {
   return (
@@ -16,6 +20,11 @@ export default function Home() {
          Thank you for visiting.
           You can contact me ASAP through email at santoshadhikari486@gmail.com
         </p>
+        <div className="links">
+          <a target="_blank" href="https://github.com/UniqueJackal5"><BsGithub size={30}/></a>                   
+          <a target="_blank" href="https://www.youtube.com/channel/UCe3q73KaQVdFVcqPuAdXQkA"><BsYoutube size={30}/></a>
+          <a target="_blank" href="https://www.instagram.com/santosh18_/"><FaInstagram size={30}/></a>	  
+        </div>
       </main>
 
       <Footer />
